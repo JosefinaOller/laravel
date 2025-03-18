6effd8354f90e7541dc863f9e2940f2e941b933d
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-03-18 19:10:28 -0300

    Creación de templates con Blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <a href="/">Inicio</a> |
+    <a href="/suma">Aplicación de la suma</a> |
+    <a href="#">Opción 3</a>
+    <br>
+    @yield('content')
+    Hola Soy templeate
+</body>
+</html>
--- resources/views/suma.blade.php
+++ resources/views/suma.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Suma de dos números</title>
-</head>
-<body>
-    <h2> Sumar dos números</h2>
+   @extends('layouts.app')
+   
+   @section('content')
+   <h2> Sumar dos números</h2>
     <form action="/suma" method="POST">
         @csrf
        
@@ -25,5 +20,5 @@
     @if(isset($resultado))
         <h3>El resultado de la suma es: {{$resultado}}</h3>
     @endif
-</body>
-</html>
+
+    @endsection
